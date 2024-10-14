df9335986eea68616e7f52b8e48e90769baead6e
Emilio24-dev <emchzz9@gmail.com>
2024-10-13 21:44:57 -0600

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 # Cargar el dataset
 df = pd.read_csv('gym.csv')
 
-# Contar la cantidad de cada tipo de workout
-alimentos = df['Workout_Type'].value_counts()
+#Definimos la longitud de nuestro grafico
+ejercicios = df['Workout_Type'].value_counts()
 
 # Crear el gráfico circular
 plt.figure(figsize=(10, 6))
-plt.pie(alimentos, labels=alimentos.index, autopct='%1.1f%%', startangle=140)
-plt.title('Alimentos')
-plt.axis('equal')  # Para que el gráfico sea un círculo
+plt.pie(ejercicios, labels=ejercicios.index, autopct='%1.1f%%', startangle=140)
+plt.title('Ejercicios')
+plt.axis('equal') #Aca definimos el tipo de grafico, en este caso circular
 plt.show()
+
+#Muestra en un grafico circular según los ejercicios más usados en el gimnasio
